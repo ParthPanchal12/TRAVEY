cb7c5b413f1041d7099c77756c8cc72ab01ed453
sarthak1996 <khandekarsarthak@gmail.com>
2016-04-10 16:29:17 +0530

    Added Custom Dialog Added on click listener

--- CustomDialog/app/src/main/java/com/example/sarthak/customdialog/MainActivity.java
+++ CustomDialog/app/src/main/java/com/example/sarthak/customdialog/MainActivity.java
@@ -1,13 +1,31 @@
 package com.example.sarthak.customdialog;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.afollestad.materialdialogs.MaterialDialog;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.temp);
+        boolean wrapInScrollView = true;
+        MaterialDialog dialog = new MaterialDialog.Builder(this)
+                .title("I am bored")
+                .customView(R.layout.activity_main, wrapInScrollView)
+                .positiveText("Add")
+                .show();
+        EditText editText = (EditText) dialog.getCustomView().findViewById(R.id.password);
+        editText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
